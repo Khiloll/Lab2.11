a6648e322324dda62664ec68333cb16fdcbbeeb8
Hilol <imanuilyusupov@gmail.com>
2024-02-18 19:45:34 +0300

    update

--- prog/ind.py
+++ prog/ind.py
@@ -1,40 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Используя замыкания функций, объявите внутреннюю функцию, которая принимает
-# в качестве аргумента список целых чисел и удаляет из него все четные или
-# нечетные значения в зависимости от значения параметра type . Если type
-# равен «even», то удаляются четные значения, иначе – нечетные. По умолчанию
-# type должно принимать значение «even». Вызовите внутреннюю функцию замыкания
-# и отобразите на экране результат ее работы.
-
-def del_items(type_param='even'):
-    """
-    Выбирается тип элементов для удаления.
-
-    Можно выбрать 'even' - удалятся четные;
-    иначе не четные. По умолчанию стоит 'even'.
-    """
-    def delete(list_items):
-        """
-        Функция удалет некоторые элементы массива в зависимости от type_param.
-
-        Type_param задается во внешней функции.
-        """
-        match type_param:
-            case 'even':
-                return list(filter(lambda x: x % 2 != 0, list_items))
-            case _:
-                return list(filter(lambda x: x % 2 == 0, list_items))
-    return delete
 
 
-if __name__ == "__main__":
-    start_items = [0, 1, 2, 3, 4, 5, 23, 21, 14, 72]
-    print(f"\n{start_items=}\n")
+def template_func(template):
+    def replace_data(last_name, first_name):
+        new_template = template.replace("%F%", last_name).replace("%N%", first_name)
+        return new_template
 
-    del_even = del_items()
-    print(f"{del_even(start_items)=}\n")
+    return replace_data
 
-    del_not_even = del_items('not even')
-    print(f"{del_not_even(start_items)=}\n")
+# Шаблон для приветствия
+template = "Уважаемый %F%, %N%! Вы делаете работу по замыканиям функций."
+
+# Создание замыкания
+greeter = template_func(template)
+
+# Ввод фамилии и имени
+last_name = input("Введите фамилию: ")
+first_name = input("Введите имя: ")
+
+# Вызов внутренней функции для замены данных в шаблоне
+result = greeter(last_name, first_name)
+
+print(result)
